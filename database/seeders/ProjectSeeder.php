a7802e42e20c6204e09f0898c30ec35602718b2f
Spacekitsune <70965432+Spacekitsune@users.noreply.github.com>
2022-11-04 18:11:23 +0200

    Edited database

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -8,6 +8,8 @@ use Illuminate\Database\Seeder;
 
 use App\Models\Project;
 
+use Illuminate\Support\Facades\DB;
+
 class ProjectSeeder extends Seeder
 {
     /**
@@ -17,7 +19,39 @@ class ProjectSeeder extends Seeder
      */
     public function run()
     {
-        Project::factory()->count(10)->create();
+        // Project::factory()->count(10)->create();
+
+        DB::table('projects')->insert([
+            'title' => 'Organized incremental function',
+            'description' => "Good-bye, feet!' (for when she next peeped out the words: 'Where's the other guinea-pig cheered, and was beating her violently with its eyelids, so he with his nose, and broke to pieces against one of the day; and this time the Queen in a ring, and.",
+            'status_id' => '3'
+        ]);
+
+        DB::table('projects')->insert([
+            'title' => 'De-engineered 4th generation core',
+            'description' => "English!' said the Gryphon, sighing in his confusion he bit a large cat which was a body to cut it off from: that he shook his grey locks, 'I kept all my limbs very supple By the use of a book,' thought Alice to herself, as well say,' added the.",
+            'status_id' => '2'
+        ]);
+
+        DB::table('projects')->insert([
+            'title' => 'Proactive fault-tolerant extranet',
+            'description' => "In another moment down went Alice after it, never once considering how in the world she was to get out again.",
+            'status_id' => '2'
+        ]);
+
+        DB::table('projects')->insert([
+            'title' => 'Managed mobile focus group',
+            'description' => "WHAT are you?' said Alice, 'because I'm not myself, you see.' 'I don't know the meaning of half those long words, and, what's more, I don't think,' Alice went timidly up to the Queen, 'and he shall tell you how it was all dark overhead; before her was.",
+            'status_id' => '1'
+        ]);
+
+        DB::table('projects')->insert([
+            'title' => 'Pre-emptive real-time monitoring',
+            'description' => "Alice, with a T!' said the Gryphon whispered in a minute or two, which gave the Pigeon the opportunity of saying to her that she wanted to send the hedgehog to, and, as the White Rabbit, 'and that's why. Pig!' She said this last word two or three times.",
+            'status_id' => '3'
+        ]);
+
+        
 
         foreach (Project::all() as $project) {
             $users=\App\Models\User::inRandomOrder()->take(rand(1,5))->pluck('id');
--- database/seeders/TaskSeeder.php
+++ database/seeders/TaskSeeder.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Seeder;
 
 use App\Models\Task;
 
+use Illuminate\Support\Facades\DB;
+
 class TaskSeeder extends Seeder
 {
     /**
@@ -15,6 +17,105 @@ class TaskSeeder extends Seeder
      */
     public function run()
     {
-        Task::factory()->count(30)->create();
+        // Task::factory()->count(30)->create();
+
+        DB::table('tasks')->insert([
+            'title' => 'Synergistic solution-oriented intranet',
+            'description' => 'Donec diam neque, vestibulum eget, vulputate ut, ultrices vel, augue.',
+            'project_id' => '1',
+            'user_id' => '1',
+            'status_id' => '3',
+            'priority_id' => '1'
+        ]);
+
+        DB::table('tasks')->insert([
+            'title' => 'Operative web-enabled installation',
+            'description' => 'Phasellus in felis. Donec semper sapien a libero. Nam dui.',
+            'project_id' => '1',
+            'user_id' => '1',
+            'status_id' => '3',
+            'priority_id' => '1'
+        ]);
+
+        DB::table('tasks')->insert([
+            'title' => 'Quality-focused multi-state project',
+            'description' => 'Duis aliquam convallis nunc. Proin at turpis a pede posuere nonummy. Integer non velit.',
+            'project_id' => '1',
+            'user_id' => '1',
+            'status_id' => '3',
+            'priority_id' => '1'
+        ]);
+
+        DB::table('tasks')->insert([
+            'title' => 'Balanced responsive matrices',
+            'description' => 'Vestibulum quam sapien, varius ut, blandit non, interdum in, ante. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Duis faucibus accumsan odio. Curabitur convallis.',
+            'project_id' => '2',
+            'user_id' => '1',
+            'status_id' => '3',
+            'priority_id' => '2'
+        ]);
+
+        DB::table('tasks')->insert([
+            'title' => 'Automated exuding Graphical User Interface',
+            'description' => 'Fusce consequat. Nulla nisl. Nunc nisl.',
+            'project_id' => '2',
+            'user_id' => '1',
+            'status_id' => '2',
+            'priority_id' => '2'
+        ]);
+
+        DB::table('tasks')->insert([
+            'title' => 'Secured background initiative',
+            'description' => 'Quisque id justo sit amet sapien dignissim vestibulum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nulla dapibus dolor vel est. Donec odio justo, sollicitudin ut, suscipit a, feugiat et, eros.',
+            'project_id' => '2',
+            'user_id' => '1',
+            'status_id' => '2',
+            'priority_id' => '2'
+        ]);
+
+        DB::table('tasks')->insert([
+            'title' => 'Configurable well-modulated approach',
+            'description' => 'In hac habitasse platea dictumst. Etiam faucibus cursus urna. Ut tellus.',
+            'project_id' => '3',
+            'user_id' => '1',
+            'status_id' => '3',
+            'priority_id' => '3'
+        ]);
+
+        DB::table('tasks')->insert([
+            'title' => 'Cross-group explicit functionalities',
+            'description' => 'Nullam sit amet turpis elementum ligula vehicula consequat. Morbi a ipsum. Integer a nibh.',
+            'project_id' => '3',
+            'user_id' => '1',
+            'status_id' => '2',
+            'priority_id' => '3'
+        ]);
+
+        DB::table('tasks')->insert([
+            'title' => 'Up-sized holistic contingency',
+            'description' => 'Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vivamus vestibulum sagittis sapien. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.',
+            'project_id' => '3',
+            'user_id' => '1',
+            'status_id' => '1',
+            'priority_id' => '3'
+        ]);
+
+        DB::table('tasks')->insert([
+            'title' => 'Profit-focused systematic hierarchy',
+            'description' => 'Sed sagittis. Nam congue, risus semper porta volutpat, quam pede lobortis ligula, sit amet eleifend pede libero quis orci. Nullam molestie nibh in lectus.',
+            'project_id' => '5',
+            'user_id' => '1',
+            'status_id' => '3',
+            'priority_id' => '1'
+        ]);
+
+        DB::table('tasks')->insert([
+            'title' => 'Business-focused encompassing capacity',
+            'description' => 'Integer tincidunt ante vel ipsum. Praesent blandit lacinia erat. Vestibulum sed magna at nunc commodo placerat.',
+            'project_id' => '5',
+            'user_id' => '1',
+            'status_id' => '3',
+            'priority_id' => '1'
+        ]);
     }
 }
